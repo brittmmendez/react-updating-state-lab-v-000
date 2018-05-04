1b107a9b1b46faf2e43a0d8703df07bd5e5d5c68
Brittany Morris <brittmmendez@gmail.com>
2018-05-04 20:44:16 +0000

    automatically backed up by learn

--- src/components/YouTubeDebugger.js
+++ src/components/YouTubeDebugger.js
@@ -1,1 +1,39 @@
-// Code YouTubeDebugger Component Here
+class YouTubeDebugger extends React.Component {
+  constructor() {
+    super();
+
+    // Define the initial state:
+    this.state = {
+      errors: [],
+      user: null,
+      settings: {
+        bitrate: 8,
+        video: {
+          resolution: '1080p'
+        }
+      }
+    };
+  }
+  
+  handleBitrate = () => {
+    this.setState({
+      settings.bitrate: 12
+    })
+  }
+  
+  handleBitrate = () => {
+    this.setState({
+      settings.video.resolution: '720p'
+    })
+  }
+
+  render() {
+    return (
+      <div>
+        <button onClick={this.handleBitrate}>bitrate</button>
+        <button onClick={this.handleResolution}>resolution</button>
+      </div>
+      
+    );
+  }
+}
